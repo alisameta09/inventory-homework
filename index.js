3b122b64fff87a9f57424b405ad53ef071cfbaee
alisameta <163756707+alisameta09@users.noreply.github.com>
2025-08-25 15:27:37 +0300

    1.2 Updated homework

--- index.js
+++ index.js
@@ -1,48 +1,73 @@
-// let inventory = [
-//     {id: 1, name: "Apple", price: 0.5, quantity: 200},
-//     {id: 2, name: "Banana", price: 0.3, quantity: 150},
-//     {id: 3, name: "Cherry", price: 0.9, quantity: 50}
-// ];
+let inventory = [
+    {id: 1, name: "Apple", price: 0.5, quantity: 200},
+    {id: 2, name: "Banana", price: 0.3, quantity: 150},
+    {id: 3, name: "Cherry", price: 0.9, quantity: 50}
+];
 
 
 // 1. Получите все продукты и выведите их названия на консоль.
 
+// 1.1
 // for (const {name: fruitName} of inventory) {
 //     console.log(fruitName);
 // }
 
-// const products = inventory.map((element) => {
-//     return element.name;
-// })
+// 1.2
+// for (let i = 0; i < inventory.length; i++) {
+//     console.log(inventory[i].name);
+// }
 
+// 1.3
+// const products = inventory.map(element => element.name);
 // console.log(products)
+
+// 1.4
+// const allProducts = inventory.map(product => product.name).join(', ');
+// console.log(`Продукты на складе: ${allProducts}.`);
 
 
 // 2. Напишите функцию updateInventory(productId, amount), которая обновляет количество определенного ID товара.
 // Количество может быть положительным (добавление к инвентарю) или отрицательным (вычитание из инвентаря).
 // Убедитесь, что функция не позволяет инвентарю опуститься ниже 0.
 
+// 2.1
 // function updateInventory(productId, amount) {
-//     const productIndex = inventory.findIndex(product => product.id === productId);
+//     let product = inventory.find(item => item.id === productId);
 //
-//     if (productIndex !== -1) {
-//         const productName = inventory[productIndex].name;
-//         let newAmount = inventory[productIndex].quantity + amount;
-//
-//         if (newAmount < 0) {
-//             newAmount = 0;
-//         }
-//         console.log(`Количество товара ${productName} изменено на ${newAmount}`);
-//     } else {
+//     if (!product) {
 //         console.log(`Товар с ID ${productId} не найден.`);
+//         return;
+//     }
+//     const productName = product.name;
+//     let newAmount = product.quantity + amount;
+//
+//     product.quantity = Math.max(0, product.quantity + amount);
+//
+//     if (newAmount < 0) {
+//         console.log(`Количество товара: ${productName} не может быть отрицательным`)
+//     } else {
+//         console.log(`Количество товара ${productName} изменено на ${newAmount}`);
+//     }
+// }
+
+// 2.2
+// let updateInventory = (productId, amount) => {
+//     let product = inventory.find(item => item.id === productId);
+//
+//     if (product) {
+//         product.quantity = Math.max(0, product.quantity + amount);
+//     } else {
+//         console.log(`Товар с ID ${productId} не найден в инвентаре.`);
 //     }
 // }
 //
-// updateInventory(3, -300);
+// updateInventory(2, -300);
+// console.log(inventory);
 
 
 // 3. Поиск товаров с низким запасом. Напишите функцию findLowStock(threshold), которая возвращает массив названий товаров, количество которых ниже определенного порога.
 
+// 3.1
 // function findLowStock(threshold) {
 //     let inventoryName = [];
 //
@@ -53,9 +78,17 @@
 //     })
 //     return inventoryName;
 // }
-//
+
 // console.log(findLowStock(200));
 
+// 3.2
+// let findLowStock = (threshold) => {
+//     return inventory
+//         .filter(item => item.quantity < threshold)
+//         .map(item => item.name);
+// };
+//
+// console.log(findLowStock(200));
 
 // 4. Меняем на объект и теперь нужно выполнить используя Object.entries() для работы с ключом и данными каждого товара.
 
@@ -75,7 +108,8 @@
 //     Banana: 150,
 //     Cherry: 50
 // };
-//
+
+// Общее кол-во запасов:
 // function totalInventoryValue() {
 //     const inventoryVal = Object.values(inventory);
 //     let total = 0;
@@ -84,6 +118,14 @@
 //         total += num;
 //     }
 //     return total;
+// }
+
+
+// Правильное задание и решение:
+// let totalInventoryValue = () => {
+//     return inventory.reduce((a, b) => {
+//         return a + (b.price * b.quantity)
+//     }, 0);
 // }
 //
 // console.log(totalInventoryValue());
